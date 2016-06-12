05812bd983b560af52822e703edaefa3978dbe68
云公司-张经纬 <18611105756@189.cn>
2016-06-12 14:08:24 +0800

    add jdk8 practises

---
+++ practises-jdk8-features/src/main/java/org/practises/jdk8/features/App.java
@@ -0,0 +1,13 @@
+package org.practises.jdk8.features;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
